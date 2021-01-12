68eb8f614bff1eccaea94665dffeb1e0cc9666cd
charconstpointer <attwistingnether@gmail.com>
2021-01-12 09:39:25 +0100

    flags

--- cmd/url-collector/main.go
+++ cmd/url-collector/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"flag"
+
 	"gitlab.com/charconstpointer/TWljaGFsIEdvZ29BcHBzIE5BU0E/pkg/pics"
 )
 
+var (
+	port   = flag.Int("port", 8080, "http port")
+	conc   = flag.Int("conc", 5, "max concurrect go routines")
+	layout = flag.String("layout", "2006-01-02", "date layout for time parsing")
+)
+
+//This project uses only go stdlib as i didn't really feel the need for external libs considering the size of this project
 func main() {
+	flag.Parse()
+
 	cfg := pics.Config{
-		Layout: "2006-01-02",
-		Conc:   5,
-		Port:   8080,
+		Layout: *layout,
+		Conc:   *conc,
+		Port:   *port,
 		Logger: pics.NewPicsLogger(),
 	}
 
